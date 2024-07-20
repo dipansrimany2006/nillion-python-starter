ed60fd7ef73ab57feb8e72294fed111d08585bd4
dipansrimany2006 <dipansrimany@gmail.com>
2024-07-20 15:27:53 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -1,25 +1,25 @@
 from nada_dsl import *
 
 def nada_main():
-    # Define parties
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
-    party4 = Party(name="Party4")
-    
-    # Define inputs from different parties
-    a = SecretInteger(Input(name="A", party=party1))
-    b = SecretInteger(Input(name="B", party=party2))
-    c = SecretInteger(Input(name="C", party=party3))
-    
-    # Perform operations
-    addition_result = a + b
-    subtraction_result = a - c
-    multiplication_result = b * c
-    
-    # Return results to a party
-    return [
-        Output(addition_result, "addition_output", party4),
-        Output(subtraction_result, "subtraction_output", party4),
-        Output(multiplication_result, "multiplication_output", party4)
-    ]
+    # 1. Parties initialization
+    contributor0 = Party(name="Contributor0")
+    contributor1 = Party(name="Contributor1")
+    contributor2 = Party(name="Contributor2")
+    outparty = Party(name="OutParty")
+
+    # 2. Inputs initialization
+    ## Contributions from contributor 0
+    c0 = SecretUnsignedInteger(Input(name="c0", party=contributor0))
+    ## Contributions from contributor 1
+    c1 = SecretUnsignedInteger(Input(name="c1", party=contributor1))
+    ## Contributions from contributor 2
+    c2 = SecretUnsignedInteger(Input(name="c2", party=contributor2))
+
+    # 3. Computation
+    ## Add all contributions
+    total_contribution = c0 + c1 + c2
+
+    # 4. Output
+    total_contribution_output = Output(total_contribution, "total_contribution", outparty)
+
+    return [total_contribution_output]
